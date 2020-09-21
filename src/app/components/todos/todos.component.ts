60804a7d65555831fd4fd940d4dc83ea4c2487ea
nathanziarczyk <nathanziarczyk1997@outlook.com>
2020-09-21 11:29:36 +0200

    todo service: DELETE

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -19,6 +19,10 @@ export class TodosComponent implements OnInit {
   }
 
   deleteTodo(todo: Todo) {
-    console.log(todo);
+    // Remove from UI
+    this.todos = this.todos.filter((t) => t.id !== todo.id);
+
+    // Remove from server
+    this.todoService.deleteTodo(todo).subscribe();
   }
 }
